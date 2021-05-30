8f6a9d524b84e9da12c0cdb4df648620c960909d
Jeral <jeral.cris@gmail.com>
2021-05-30 16:11:02 -0600

    Nuevo servicio validarJWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,16 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarCampos, validarJWT } = require('../middlewares');
 
-const { login, googleSignIn } = require('../controllers/auth');
+const { login, googleSignIn, renovarToken } = require('../controllers/auth');
 
 const router = Router();
 
 router.post('/login', [check('correo', 'El correo no es valido').isEmail(), check('password', 'La contraseña es obligatoria').not().isEmpty(), validarCampos], login);
 
 router.post('/google', [check('id_token', 'El id_token es necesario').not().isEmpty(), validarCampos], googleSignIn);
+
+router.get('/', [validarJWT], renovarToken);
 
 module.exports = router;
